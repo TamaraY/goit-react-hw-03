d4d6b874d2157b5c18e32616b7539998eae9f290
TamaraY <tamasik99@gmail.com>
2024-09-25 11:51:01 +0300

    updated  according to the comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { nanoid } from "nanoid";
 
 import SearchBox from "./components/SearchBox/SearchBox";
@@ -6,14 +6,25 @@ import СontactList from "./components/ContactList/ContactList";
 import ContactForm from "./components/ContactForm/ContactForm";
 
 const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-  ]);
+  const [contacts, setContacts] = useState(() => {
+    // updated  according to the comments
+    const storedContacts = localStorage.getItem("contacts");
+    return storedContacts
+      ? JSON.parse(storedContacts)
+      : [
+          { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+          { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+          { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+          { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+        ];
+  });
 
   const [filter, setFilter] = useState("");
+
+  // updated  according to the comments
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
 
   const handleFilterChange = (newFilter) => {
     setFilter(newFilter);
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,26 +1,20 @@
+import Contact from "../Contact/Contact";
 import style from "./ContactList.module.css";
 
-const List = ({ data, handleDelete }) => {
+const ContactList = ({ data, handleDelete }) => {
   return (
-    <div className={style.listContact}>
+    <div>
       <ul className={style.list}>
         {data.map((contact) => (
-          <li key={contact.id} className={style.item}>
-            <div className={style.contact}>
-              <p className={style.contactText}> {contact.name}</p>
-              <p className={style.contactText}> {contact.number}</p>
-            </div>
-            <button
-              className={style.btn}
-              onClick={() => handleDelete(contact.id)}
-            >
-              Delete
-            </button>
-          </li>
+          <Contact
+            key={contact.id}
+            contact={contact}
+            handleDelete={handleDelete}
+          />
         ))}
       </ul>
     </div>
   );
 };
 
-export default List;
+export default ContactList;
